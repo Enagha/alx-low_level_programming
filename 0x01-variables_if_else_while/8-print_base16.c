62ae93f84484d130ce425ebd6319dc0946606405
Enagha <enagha2014@gmail.com>
2022-09-10 16:47:51 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,9 +13,8 @@ int main(void)
 	for (letter = 'a'; letter <= 'f'; letter++)
 		putchar(letter);
 
-	for (num ='0'; num <10; num++)
+	for (num = '0'; num < 10; num++)
 		putchar((num));	
-	
 	putchar('\n');
 	return (0);
 }
