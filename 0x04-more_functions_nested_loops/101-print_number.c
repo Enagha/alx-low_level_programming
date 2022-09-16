40647b0ed4d7fd344b968c7cdf4258e44c2c6105
Enagha <enagha2014@gmail.com>
2022-09-16 14:32:10 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,6 +1,7 @@
-include "main.h" 
-/** 
- * print_number - prints an integer 
+#include "main.h"
+
+/**
+ * print_number - prints an integer
  * @n: integer to be printed
  */
 void print_number(int n)
