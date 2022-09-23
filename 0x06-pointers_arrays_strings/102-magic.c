39a7b9d9f6ce2fcf75ca9f5a949be105e28c9884
Enagha <enagha2014@gmail.com>
2022-09-23 09:43:00 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -3,7 +3,7 @@
 int main(void)
 {
   int n;
-  int a[2];
+  int a[5];
   int *p;
 
   a[2] = 1024;
@@ -20,4 +20,4 @@ int main(void)
   /* ...so that this prints 98\n */
   printf("a[2] = %d\n", a[2]);
   return (0);
- }
+}
