26c539ba869859659f7a84ea29d984947c53df30
Enagha <enagha2014@gmail.com>
2022-10-10 05:11:18 -0700

    dog.h

---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,9 @@
+#include MAIN_H
+#include MAIN_H
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+endif 
