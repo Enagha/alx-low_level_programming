da3b1c881bf2f33a0fe1209e93362075fdcdbe05
Enagha <enagha2014@gmail.com>
2022-10-16 12:53:54 -0700

    3-print_all.c

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef VA_FUNCT_H
+#define VA_FUNCT_H
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
