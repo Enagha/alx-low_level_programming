f2fbd38cd947fbf0b66e7319981eeae07f230d37
Enagha <enagha2014@gmail.com>
2022-10-25 09:24:28 -0700

    function that prints all the elements of a listint_t

---
+++ 0x13-more_singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include"lists.h"
+
+/**
+*print_listint-printsalltheelementsofalinkedlist
+*@h:pointertothe1stnodeofthelinkedlist
+*Return:numberofnodes
+*/
+
+size_tprint_listint(constlistint_t*h)
+
+{
+
+size_tnodenum=0;
+
+
+
+	while(h)
+
+	{	
+
+		printf("%d\n",h->n);
+
+		nodenum++;	
+
+		h=h->next;
+
+	}	
+
+	return(nodenum);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+Second Linked Lists Task
