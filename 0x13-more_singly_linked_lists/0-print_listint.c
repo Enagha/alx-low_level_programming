10fde676ddf401868deda5d44980a738d2099224
Enagha <enagha2014@gmail.com>
2022-10-26 04:58:53 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,19 +8,18 @@
 *Return:numberofnodes
 */
 
-size_t print_listint(const listint_t*h)
+size_t print_listint(const listint_t *h)
 {
 
 size_t nodenum = 0;
 
 
-	while(h)
-
-	{	
-		printf("%d\n",h->n);
+	while (h)
+	{
+		printf("%d\n", h->n);
 		nodenum++;
 		h = h->next;
-	}	
+	}
 
 	return (nodenum);
 }
