440463a01263ca29f779d055549197bf73248fa2
Enagha <enagha2014@gmail.com>
2022-10-26 05:48:48 -0700

    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

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,17 +1,15 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include"lists.h"
 /**
 *print_listint-printsalltheelementsofalinkedlist
 *@h:pointertothe1stnodeofthelinkedlist
+
 *Return:numberofnodes
 */
 
 size_t print_listint(const listint_t *h)
 {
 
-size_t num = 0;
+	size_t num = 0;
 
 
 	while (h)
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,8 +1,10 @@
 #ifndef LISTS_H 
 #define LISTS_H
+
 #include <stdlib.h>
 #include <stdio.h>
-/**
+
+ /**
  *struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
